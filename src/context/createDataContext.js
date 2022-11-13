8b7b019354c1b28dade29aa362a7fb693b1c797e
Swag Finger <swagfinger@gmail.com>
2022-11-13 09:31:21 +0800

    typo

--- src/context/createDataContext.js
+++ src/context/createDataContext.js
@@ -7,7 +7,7 @@ export default (reducer, actions, defaultValue) => {
   const Provider = ({ children }) => {
     const [state, dispatch] = useReducer(reducer, defaultValue);
 
-    const boundAction = {};
+    const boundActions = {};
     for (let key in actions) {
       boundActions[key] = actions[key](dispatch); //call each key with "key"(dispatch)
     }
