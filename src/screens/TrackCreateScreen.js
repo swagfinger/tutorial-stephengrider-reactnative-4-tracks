04722b4b4cb7db02436ce25445d0201d9b89c122
Swag Finger <swagfinger@gmail.com>
2022-11-15 23:26:39 +0800

    requestForegroundPermissionsAsync works - asks for permission to get location

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from '@rneui/themed';
 import Map from '../components/Map';
 //safearea
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { requestForegroundPermissionsAsync } from 'expo-location'; //requestPermissions is deprecated
+
 const TrackCreateScreen = () => {
+  const [err, setErr] = useState(null);
+
+  const startWatching = async () => {
+    try {
+      const { granted } = await requestForegroundPermissionsAsync(); //request in the startWatching method
+      if (!granted) {
+        throw new Error('Location permission not granted');
+      }
+    } catch (e) {
+      setErr(e);
+    }
+  };
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+
   return (
     <SafeAreaView>
       <Text h2>Create a track</Text>
       <Map />
+      {err ? <Text>Please enable location services</Text> : null}
     </SafeAreaView>
   );
 };
