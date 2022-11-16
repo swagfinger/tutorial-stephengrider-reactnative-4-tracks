62de908e877babb688a6c48b2c60394b85f2bd58
Swag Finger <swagfinger@gmail.com>
2022-11-17 00:58:21 +0800

    renders paths

--- src/screens/TrackDetailScreen.js
+++ src/screens/TrackDetailScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import { Context as TrackContext } from '../context/TrackContext';
+import MapView, { Polyline } from 'react-native-maps';
 
 const TrackDetailScreen = ({ route, navigation }) => {
   const { state } = useContext(TrackContext);
@@ -8,10 +9,28 @@ const TrackDetailScreen = ({ route, navigation }) => {
 
   //find a track
   const track = state.find((t) => t._id === _id);
-
-  return <Text style={{ fontSize: 48 }}>{track.name}</Text>;
+  const initialCoords = track.locations[0].coords;
+  return (
+    <>
+      <Text style={{ fontSize: 48 }}>{track.name}</Text>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          longitudeDelta: 0.01, //zoom
+          latitudeDelta: 0.01, //zoom
+          ...initialCoords
+        }}
+      >
+        <Polyline coordinates={track.locations.map((loc) => loc.coords)} />
+      </MapView>
+    </>
+  );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  map: {
+    height: 300
+  }
+});
 
 export default TrackDetailScreen;
